988e55e687c1060b172ca5343c14f4c5579da54e
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2025-01-21 23:17:11 +0800

    chore: Removing binary crate

--- src/main.rs
+++
@@ -1,7 +0,0 @@
-use disperror::DispError;
-
-fn main() -> Result<(), DispError<std::io::Error>> {
-    let contents = std::fs::read_to_string("nonexistent_file.txt")?;
-    println!("{}", contents);
-    Ok(())
-}
